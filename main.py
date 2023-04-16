e226b0ea655f906006ef4350193aab19016569e6
carter <66398300+carteruh@users.noreply.github.com>
2023-04-16 03:35:14 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import cv2
 import mediapipe as mp
 import time
 import csv
+import socket
+import pyautogui
+
 cap = cv2.VideoCapture(0)
 
+# TCP_IP = '127.0.0.1'
+# TCP_PORT = 135
+
+# Create a TCP socket
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect((TCP_IP, TCP_PORT))
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(static_image_mode=False,
-                      max_num_hands=2,
+                      max_num_hands=4,
                       min_detection_confidence=0.5,
                       min_tracking_confidence=0.5)
 mpDraw = mp.solutions.drawing_utils
@@ -21,14 +30,33 @@ while True:
     #print(results.multi_hand_landmarks)
 
     if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
+        for idx, handLms in enumerate(results.multi_hand_landmarks):
             for id, lm in enumerate(handLms.landmark):
                 #print(id,lm)
-                h, w, c = img.shape
-                cx, cy = int(lm.x *w), int(lm.y*h)
-                print(cx, cy)
-                #if id ==0:
-                cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
+                if (id == 8):
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x *w), int(lm.y*h)
+                    print(cx, cy)
+                    if (lm.x > 0.65):
+                        pyautogui.keyDown("left")
+                        pyautogui.keyUp("right")
+                    elif (lm.x < .65 and lm.x > .35):
+                        pyautogui.keyUp("left")
+                        pyautogui.keyUp("right")
+                    else:
+                        pyautogui.keyUp("left")
+                        pyautogui.keyDown("right")
+                    # if (idx % 5 == 0):
+                    #     with open("C:/CV_Development/Pose_estimation_Hand/yolov5/lm_hand.txt", "w", newline= '') as f:
+                    #         cx_text, cy_text = str(cx), str(cy)
+                    #         f.write(cx_text  + ',' + cy_text)
+                    cx_text, cy_text = str(cx), str(cy)
+                    message = cx_text + "_" + cy_text
+                    # s.sendall(message.encode("utf-8"))
+                    # response = s.recv(1024).decode("utf-8")
+                    # print(response)
+                    #if id ==0:
+                    cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
 
 
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
@@ -46,3 +74,4 @@ while True:
 # Release the video capture device and close all windows
 cap.release()
 cv2.destroyAllWindows()
+# s.close()
